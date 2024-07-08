60551465cc0adbc42611fb6abfb5b745a2b174ca
Shahmeer Nasir <155984563+shahmeer-technyx@users.noreply.github.com>
2024-07-08 16:09:34 +0500

    Update GTM.js

--- src/components/GTM/GTM.js
+++ src/components/GTM/GTM.js
@@ -9,12 +9,7 @@ const GTM = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.innerHTML = `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NTHL37DM');
-    `;
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NTHL37DM');`;
     document.head.appendChild(script);
 
     return () => {
@@ -23,8 +18,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   }, []);
 
   return (
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTHL37DM"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTHL37DM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
   );
 };
 
