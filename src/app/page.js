7ca3e9ecaba36039ec82a0e520d0976f0498828e
Shahmeer Nasir <155984563+shahmeer-technyx@users.noreply.github.com>
2024-07-08 17:11:23 +0500

    Added dataLayer

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
+"use client";
 
 import CourseList from '@/components/CourseList';
 import GTM from '@/components/GTM/GTM';
 import { COURSES } from '@/data';
 
 export default function Page() {
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dataLayer.push({
+      'event': 'formSubmit',
+      'formId': 'contactForm'
+    });
+  }
   return (
     <>
       <GTM />
       <CourseList courses={COURSES} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   )
 };
