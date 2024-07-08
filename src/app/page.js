137eb48b61dcdf369e341cd78ff08e1e112d0179
Shahmeer Nasir <155984563+shahmeer-technyx@users.noreply.github.com>
2024-07-08 16:05:58 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 
 import CourseList from '@/components/CourseList';
-import GTM from '@/components/GTM/GTM';
 import { COURSES } from '@/data';
 
 export default function Page() {
   return (
     <>
-      <GTM />
       <CourseList courses={COURSES} />
     </>
   )
